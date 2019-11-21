4786ef6b0f7b8999d282ae1f744068f726369c86
Nathan I N Daley <ndaley7@gatech.edu>
2019-11-21 13:59:35 -0500

    Corrected Author of Bodyslide
    Changed from Caliente (CBBE and TexBlend) to Ousnius.

--- BSGG/ParsingBSGG/XMLIO.py
+++ BSGG/ParsingBSGG/XMLIO.py
@@ -1,22 +1,55 @@
+"""
+    This file is part of:
+    NIND's BodySlide Utilites
+    by NIND
 
+    Created to work with BodySlide 2 and Outfit Studio
+    by Ousnius
+
+    This software is provided 'as-is', without any express or implied
+    warranty. In no event will the authors be held liable for any
+    damages arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any
+    purpose, including commercial applications, and to alter it and
+    redistribute it freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must
+    not claim that you wrote the original software. If you use this
+    software in a product, an acknowledgment in the product documentation
+    would be appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and
+    must not be misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+"""
 #import lxml
 import lxml.etree as ET
 
 
+#Abbreviated Version of the disclaimer
+liscenceAbbrev='placeholder'
 
 def CreateConfigBSGGXML(bodyslideFilepath,debugLogStatus):
     #Intiate Root
     rootConfigXML=ET.Element('SliderGroupGeneratorConfig')
+    pathComment = ET.Comment('File paths for the Bodyslide folders important to this tool. \n Note they all use / end with an occurence of /')
 
+    rootConfigXML.append(pathComment)
     #Add in Subfolder Paths
 
-    ET.SubElement(rootConfigXML,'SliderGroupsPath').text=bodyslideFilepath+str("/SliderGroups")
-    ET.SubElement(rootConfigXML,'SliderSetsPath').text=bodyslideFilepath+str("/SliderSets")
-    ET.SubElement(rootConfigXML,'SliderCategoriesPath').text=bodyslideFilepath+str("/SliderCategories")
-    ET.SubElement(rootConfigXML,'SliderPresetsPath').text=bodyslideFilepath+str("/SliderPresets")
+    ET.SubElement(rootConfigXML,'SliderGroupsPath').text=bodyslideFilepath+str("/SliderGroups/")
+    ET.SubElement(rootConfigXML,'SliderSetsPath').text=bodyslideFilepath+str("/SliderSets/")
+    ET.SubElement(rootConfigXML,'SliderCategoriesPath').text=bodyslideFilepath+str("/SliderCategories/")
+    ET.SubElement(rootConfigXML,'SliderPresetsPath').text=bodyslideFilepath+str("/SliderPresets/")
+
+    finalpath=ET.Element('SliderPresetsPath')
 
     #Add in DebugStatus
-    ET.Comment('Enable the below option for Verbose Debugging Log')
+    errorComment=ET.Comment('Enable the below option for Verbose Debugging Log (True/False)')
+    finalpath.append(errorComment)
     ET.SubElement(rootConfigXML,'EnableDebugBSGG').text=str(debugLogStatus)
     
     #Write out the XML file
